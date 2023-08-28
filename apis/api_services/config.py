f66a7dad884d22219dcba5a40f9a7640251cd529
gbenouga <edorhlaurent@gmail.com>
2023-08-28 21:44:20 +0000

    configurationet phase de mise en place des elements neccessaires pour le projet

---
+++ apis/api_services/config.py
@@ -0,0 +1,10 @@
+# Definissons les constants de l'api existants
+
+
+HOST_URL = "https://miabeCampus.com/api"
+FLASHINFOS_URL = HOST_URL + ""
+
+APISETU_HEADERS = {
+    
+}
+
--- miabeCampusBot/settings.py
+++ miabeCampusBot/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mcapi'
 ]
 
 ROOT_URLCONF = 'miabeCampusBot.urls'
